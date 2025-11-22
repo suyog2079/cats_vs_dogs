e16430511bcf1f0f74b92299d52b118c1b9ed722
suyog2079 <079bct091.suyog@pcampus.edu.np>
2025-11-22 18:10:28 +0545

    fixed a ton of bugs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 
 fn main() {
     let mut choice = String::new();
-    println!("0 for train, 1 for test: ");
+    println!("0 for train, 1 for test, 2 for evaluation: ");
 
     io::stdin()
         .read_line(&mut choice)
@@ -16,9 +16,28 @@ fn main() {
     let choice: u32 = choice.trim().parse().expect("Please type a number!");
     if choice == 0 {
         train();
-    } else {
+    } else if choice == 1 {
         test();
+    } else {
+        evaluate();
     }
+
+    // let (img, label) = get_train_image_vector(100);
+    // println!("Image size: {}", img.len());
+}
+
+fn evaluate() {
+    let theta: &mut Vec<f32> = &mut vec![0.0; 80601];
+    read_theta(theta);
+    let mut correct = 0;
+    for i in 0..100 {
+        let (inp, label) = get_test_image_vector(i);
+        let prediction = compute(&inp, theta);
+        if prediction == label {
+            correct += 1;
+        }
+    }
+    println!("Accuracy: {}%", correct as f32 / 10.0);
 }
 
 fn compute(inp: &[f32], theta: &mut Vec<f32>) -> i32 {
@@ -39,12 +58,12 @@ fn perceptron(inp: &[f32], theta: &mut Vec<f32>, label: i32) {
 }
 
 fn train() {
-    let mut theta = vec![0.0; 50025002];
+    let mut theta = vec![0.0; 80601];
     generate_random_theta(&mut theta);
     for i in 0..1000 {
         let (inp, label) = get_train_image_vector(i);
         perceptron(&inp, &mut theta, label);
-        println!("{}\n",i);
+        println!("{}\n", i);
     }
     write_theta(&theta);
 }
@@ -59,15 +78,16 @@ fn get_train_image_vector(i: i32) -> (Vec<f32>, i32) {
         path = format!("dataset/dog.{}.png", 3500 + (i - 1) / 2);
         label = -1; // dog is -1 
     }
-    let img = image::open(&Path::new(&path)).unwrap();
-    let mut inp: Vec<f32> = img.as_bytes().iter().map(|&p| p as f32).collect();
-    normalize(&mut inp);
+    let img = image::open(&Path::new(&path)).unwrap().to_luma8();
+    let img = image::imageops::resize(&img, 20, 20, imageops::FilterType::Gaussian);
+    let mut inp: Vec<f32> = img.as_raw().iter().map(|&p| p as f32).collect();
     let original_len = inp.len();
     for i in 0..original_len {
         for j in i..original_len {
             inp.push(inp[i] * inp[j]);
         }
     }
+    normalize(&mut inp);
     (inp, label)
 }
 
@@ -81,15 +101,16 @@ fn get_test_image_vector(i: i32) -> (Vec<f32>, i32) {
         path = format!("dataset/dog.{}.png", (i - 1) / 2);
         label = -1; // dog is -1 
     }
-    let img = image::open(&Path::new(&path)).unwrap();
-    let mut inp: Vec<f32> = img.as_bytes().iter().map(|&p| p as f32).collect();
-    normalize(&mut inp);
+    let img = image::open(&Path::new(&path)).unwrap().to_luma8();
+    let img = image::imageops::resize(&img, 20, 20, imageops::FilterType::Gaussian);
+    let mut inp: Vec<f32> = img.as_raw().iter().map(|&p| p as f32).collect();
     let original_len = inp.len();
     for i in 0..original_len {
         for j in i..original_len {
             inp.push(inp[i] * inp[j]);
         }
     }
+    normalize(&mut inp);
     (inp, label)
 }
 
@@ -98,6 +119,7 @@ fn normalize(data: &mut Vec<f32>) {
         data[i] = 2.0 * data[i] / 255.0 - 1.0;
     }
     data.push(1.0); // bias term
+    // println!("length: {}", data.len());
 }
 
 fn generate_random_theta(theta: &mut Vec<f32>) {
@@ -106,7 +128,7 @@ fn generate_random_theta(theta: &mut Vec<f32>) {
     let range = rand::distr::Uniform::new(-0.01, 0.01).unwrap();
     *theta = (&mut rng)
         .sample_iter(range)
-        .take(50025002)
+        .take(80601)
         .collect::<Vec<f32>>();
 }
 
@@ -114,7 +136,7 @@ fn test() {
     let mut index = String::new();
     let label: i32;
     let inp: Vec<f32>;
-    let theta: &mut Vec<f32> = &mut vec![0.0; 50025002];
+    let theta: &mut Vec<f32> = &mut vec![0.0; 80601];
     println!("enter index: ");
 
     io::stdin()
@@ -125,36 +147,35 @@ fn test() {
     (inp, label) = get_test_image_vector(index);
     read_theta(theta);
     let prediction = compute(&inp, theta);
-    println!("Prediction: {}, Actual: {}", prediction, label);
     if label == 1 {
-        println!("Actual: Cat")
+        println!("Actual: Cat");
     } else {
         println!("Actual: Dog")
+    }
+    if prediction == 1 {
+        println!("Prediction: Cat");
+    } else {
+        println!("Prediction: Dog");
     }
 }
 
 fn read_theta(theta: &mut Vec<f32>) {
-    let path = Path::new("theta.txt");
-
-    let buf: Vec<u8> = std::fs::read(path).unwrap();
+    let buf = std::fs::read("theta.bin").expect("Cannot read theta.bin");
 
     *theta = buf
         .chunks_exact(4)
         .map(|chunk| {
-            let bytes = chunk.try_into().expect("Chunk size is not 4 bytes");
-            f32::from_le_bytes(bytes)
+            let arr: [u8; 4] = chunk.try_into().unwrap();
+            f32::from_le_bytes(arr)
         })
         .collect();
 }
 
 fn write_theta(theta: &Vec<f32>) {
-    let path = Path::new("theta.txt");
-    let mut file = File::create(path).expect("Cannot create theta.txt");
+    let mut file = File::create("theta.bin").expect("Cannot create theta.bin");
 
-    for (i, val) in theta.iter().enumerate() {
-        if i > 0 {
-            write!(file, " ").unwrap(); // space-separated
-        }
-        write!(file, "{}", val).unwrap();
+    for &val in theta {
+        let bytes = val.to_le_bytes(); // f32 → [u8; 4]
+        file.write_all(&bytes).unwrap();
     }
 }
