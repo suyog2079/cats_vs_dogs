eb0eca3a6925a5b2c976401a4b4566c8f0902c5f
suyog2079 <079bct091.suyog@pcampus.edu.np>
2025-11-28 15:04:06 +0545

    changed perceptron to logistic regression with gradient descent

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use image::{DynamicImage, GenericImageView, ImageFormat, imageops};
+use image::imageops;
 use rand::Rng;
 use std::fs::File;
-use std::io::{self, Read};
-use std::io::{BufWriter, Write};
+use std::io::Write;
+use std::io::{self};
 use std::path::Path;
+
+// const IMG_SIZE: usize = 20 * 20; // 400
+// const POLY_SIZE: usize = IMG_SIZE * (IMG_SIZE + 1) / 2; // 80400
+// const TOTAL_FEATURES: usize = 401; // 80401
 
 fn main() {
     let mut choice = String::new();
@@ -27,43 +31,50 @@ fn main() {
 }
 
 fn evaluate() {
-    let theta: &mut Vec<f32> = &mut vec![0.0; 80601];
+    let (inp, _label) = get_test_image_vector(0);
+    let theta = &mut vec![0.0; inp.len()];
     read_theta(theta);
     let mut correct = 0;
     for i in 0..100 {
         let (inp, label) = get_test_image_vector(i);
-        let prediction = compute(&inp, theta);
+        let prediction = if compute(&inp, theta) >= 0.5 { 1 } else { 0 };
         if prediction == label {
             correct += 1;
         }
     }
-    println!("Accuracy: {}%", correct as f32 / 10.0);
+    println!("Accuracy: {}%", correct as f32);
 }
 
-fn compute(inp: &[f32], theta: &mut Vec<f32>) -> i32 {
-    let mut sum = 0.0;
-    for i in 0..inp.len() {
-        sum += inp[i] * theta[i];
-    }
-    if sum <= 0.0 { -1 } else { 1 }
+fn sigmoid(x: f32) -> f32 {
+    1.0 / (1.0 + (-x).exp())
 }
 
-fn perceptron(inp: &[f32], theta: &mut Vec<f32>, label: i32) {
-    let result = compute(inp, theta);
-    if result != label {
-        for i in 0..inp.len() {
-            theta[i] += (label as f32) * inp[i];
-        }
-    }
+fn compute(inp: &[f32], theta: &[f32]) -> f32 {
+    let sum: f32 = inp.iter().zip(theta.iter()).map(|(x, t)| x * t).sum();
+
+    sigmoid(sum)
 }
 
 fn train() {
-    let mut theta = vec![0.0; 80601];
-    generate_random_theta(&mut theta);
-    for i in 0..1000 {
-        let (inp, label) = get_train_image_vector(i);
-        perceptron(&inp, &mut theta, label);
-        println!("{}\n", i);
+    let (inp, _label) = get_train_image_vector(0);
+    let theta = &mut vec![0.0; inp.len()];
+    let mut guess: f32;
+    let mut sum: Vec<f32> = vec![0.0; inp.len()];
+
+    let epochs = 100;
+    let lambda = 0.01;
+    let eta = 0.1;
+    generate_random_theta(theta);
+    for j in 0..epochs {
+        println!("{}", j + 1);
+        for i in 0..1000 {
+            let (inp, label) = get_train_image_vector(i);
+            guess = compute(&inp, theta);
+            sum = sum.iter().zip(inp.iter()).map(|(s, &x)| s + x * (guess - label as f32)).collect(); 
+        }
+        for k in 0..theta.len() {
+            theta[k] -= eta * sum[k] / 1000.0 - eta * lambda * theta[k]; 
+        }
     }
     write_theta(&theta);
 }
@@ -73,21 +84,22 @@ fn get_train_image_vector(i: i32) -> (Vec<f32>, i32) {
     let label: i32;
     if i % 2 == 0 {
         path = format!("dataset/cat.{}.png", 3500 + i / 2);
-        label = 1; // cat is +1
+        label = 0; // cat is 0 
     } else {
         path = format!("dataset/dog.{}.png", 3500 + (i - 1) / 2);
-        label = -1; // dog is -1 
+        label = 1; // dog is 1 
     }
     let img = image::open(&Path::new(&path)).unwrap().to_luma8();
-    let img = image::imageops::resize(&img, 20, 20, imageops::FilterType::Gaussian);
+    let img = image::imageops::resize(&img, 30, 30, imageops::FilterType::Gaussian);
     let mut inp: Vec<f32> = img.as_raw().iter().map(|&p| p as f32).collect();
     let original_len = inp.len();
+    normalize(&mut inp);
     for i in 0..original_len {
         for j in i..original_len {
             inp.push(inp[i] * inp[j]);
         }
     }
-    normalize(&mut inp);
+    inp.push(1.0); // bias term
     (inp, label)
 }
 
@@ -96,21 +108,22 @@ fn get_test_image_vector(i: i32) -> (Vec<f32>, i32) {
     let label: i32;
     if i % 2 == 0 {
         path = format!("dataset/cat.{}.png", i / 2);
-        label = 1; // cat is +1
+        label = 0; // cat is 0 
     } else {
         path = format!("dataset/dog.{}.png", (i - 1) / 2);
-        label = -1; // dog is -1 
+        label = 1; // dog is 1 
     }
     let img = image::open(&Path::new(&path)).unwrap().to_luma8();
-    let img = image::imageops::resize(&img, 20, 20, imageops::FilterType::Gaussian);
+    let img = image::imageops::resize(&img, 30, 30, imageops::FilterType::Gaussian);
     let mut inp: Vec<f32> = img.as_raw().iter().map(|&p| p as f32).collect();
     let original_len = inp.len();
+    normalize(&mut inp);
     for i in 0..original_len {
         for j in i..original_len {
             inp.push(inp[i] * inp[j]);
         }
     }
-    normalize(&mut inp);
+    inp.push(1.0); // bias term
     (inp, label)
 }
 
@@ -118,17 +131,16 @@ fn normalize(data: &mut Vec<f32>) {
     for i in 0..data.len() {
         data[i] = 2.0 * data[i] / 255.0 - 1.0;
     }
-    data.push(1.0); // bias term
-    // println!("length: {}", data.len());
 }
 
 fn generate_random_theta(theta: &mut Vec<f32>) {
     let mut rng = rand::rng();
+    let (inp, _label) = get_train_image_vector(0);
 
     let range = rand::distr::Uniform::new(-0.01, 0.01).unwrap();
     *theta = (&mut rng)
         .sample_iter(range)
-        .take(80601)
+        .take(inp.len())
         .collect::<Vec<f32>>();
 }
 
@@ -136,7 +148,6 @@ fn test() {
     let mut index = String::new();
     let label: i32;
     let inp: Vec<f32>;
-    let theta: &mut Vec<f32> = &mut vec![0.0; 80601];
     println!("enter index: ");
 
     io::stdin()
@@ -145,14 +156,15 @@ fn test() {
 
     let index: i32 = index.trim().parse().expect("Please type a number!");
     (inp, label) = get_test_image_vector(index);
+    let theta = &mut vec![0.0; inp.len()];
     read_theta(theta);
     let prediction = compute(&inp, theta);
-    if label == 1 {
+    if label == 0 {
         println!("Actual: Cat");
     } else {
         println!("Actual: Dog")
     }
-    if prediction == 1 {
+    if prediction < 0.5 {
         println!("Prediction: Cat");
     } else {
         println!("Prediction: Dog");
